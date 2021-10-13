fb712431dab06bbe47ebd2941d48a3cdacaefd93
gjurajl <77977376+gjurajl@users.noreply.github.com>
2021-10-13 16:46:35 -0400

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -1,37 +1,42 @@
 import pygame
+def main():
 
-pygame.init()
+    pygame.init()
 
-pygame.display.set_caption("Lab2")
-WIDTH = 800
-HEIGHT = 480
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-screen.fill((255,0,0))
-pygame.display.update()
+    pygame.display.set_caption("Lab2 - Luis Gjuraj")
+    WIDTH = 800
+    HEIGHT = 480
+    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    screen.fill((255,0,0))
+    pygame.display.update()
 
-#WALLS
-wcolor = pygame.Color("purple")
-BORDER = 20
-#top wall
-# Rect((left, top), (width, height)) -> Rect
-pygame.draw.rect(screen, wcolor, pygame.Rect((0,0),(WIDTH,BORDER)) )
-#left wall
-pygame.draw.rect(screen, wcolor, pygame.Rect((0,0),(BORDER,HEIGHT)) )
-#bottom wall
-pygame.draw.rect(screen, wcolor, pygame.Rect((20,460),(780,20)) )
-# pygame.draw.rect(screen, wcolor, pygame.Rect(?) )
+    #WALLS
+    wcolor = pygame.Color("purple")
+    BORDER = 20
+    #top wall
+    # Rect((left, top), (width, height)) -> Rect
+    pygame.draw.rect(screen, wcolor, pygame.Rect((0,0),(WIDTH,BORDER)) )
+    #left wall
+    pygame.draw.rect(screen, wcolor, pygame.Rect((0,0),(BORDER,HEIGHT)) )
+    #bottom wall
+    pygame.draw.rect(screen, wcolor, pygame.Rect((20,460),(780,20)) )
+    # pygame.draw.rect(screen, wcolor, pygame.Rect(?) )
 
-pygame.display.update()
+    pygame.display.update()
 
 
-# define a variable to control the main loop
-running = True
-    
-# main loop
-while running:
-    # event handling, gets all event from the event queue
-    for event in pygame.event.get():
-        # only do something if the event is of type QUIT
-        if event.type == pygame.QUIT:
-            # change the value to False, to exit the main loop
-            running = False
+    # define a variable to control the main loop
+    running = True
+        
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+            
+if __name__=="__main__":
+    # call the main function
+    main()
