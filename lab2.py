2b86356d3d8e7fa4918d71f5f0b146f333f391dd
gjurajl <77977376+gjurajl@users.noreply.github.com>
2021-10-13 15:54:34 -0400

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,36 @@
+import pygame
+
+pygame.init()
+
+pygame.display.set_caption("Lab2")
+WIDTH = 800
+HEIGHT = 480
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.update()
+
+#WALLS
+wcolor = pygame.Color("purple")
+BORDER = 20
+#top wall
+# Rect((left, top), (width, height)) -> Rect
+pygame.draw.rect(screen, wcolor, pygame.Rect((0,0),(WIDTH,BORDER)) )
+#left wall
+pygame.draw.rect(screen, wcolor, pygame.Rect((0,0),(BORDER,HEIGHT)) )
+#bottom wall
+pygame.draw.rect(screen, wcolor, pygame.Rect((20,460),(780,20)) )
+# pygame.draw.rect(screen, wcolor, pygame.Rect(?) )
+
+pygame.display.update()
+
+
+# define a variable to control the main loop
+running = True
+    
+# main loop
+while running:
+    # event handling, gets all event from the event queue
+    for event in pygame.event.get():
+        # only do something if the event is of type QUIT
+        if event.type == pygame.QUIT:
+            # change the value to False, to exit the main loop
+            running = False
